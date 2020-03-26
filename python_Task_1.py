89bb3dcf83274f25616dff8ce16fc9abeecd376d
adefolawe <61995500+adefolawe@users.noreply.github.com>
2020-03-26 04:48:54 +0100

    Add files via upload

---
+++ python_Task_1.py
@@ -0,0 +1,6 @@
+Prog_name = "Python progam for calculating the area of a circle"
+print(Prog_name)
+
+from math import pi
+radius = float(input ("Input the radius of the circle : "))
+print ("The area of the circle is = " + str(pi * radius**2))
